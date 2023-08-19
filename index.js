db040597c41d07fedf4ab04c866ab27a973e876c
NasrulDev77 <142682954+NasrulDev77@users.noreply.github.com>
2023-08-19 23:46:24 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Fungsi yang berdiri sendiri
+function sapa() {
+  return "Selamat Pagi!";
+}
+
+console.log(sapa()); // Output: Selamat Pagi!
+
+// Fungsi disimpan di dalam variabel
+
+let berkenalan = function() {
+  return "Hallo, nama saya Sarah.";
+};
+
+console.log(berkenalan()); // Output: Hallo, nama saya Sarah.
